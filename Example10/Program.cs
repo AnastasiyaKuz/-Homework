aaf14e885760e595f5b0b9840eb2415b986c3887
Anastasiya <kuznecovaas94@mail.ru>
2023-04-03 13:21:28 +0300

    Homework 2

---
+++ Example10/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное
+ число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+ */
+
+Console.Write("Введите 3-х значное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int Length = num.ToString().Length;
+if (Length == 3)
+{
+    int result = (num/10) % 10;
+    Console.WriteLine("Вторая цифра введенного числа " + result);
+}
+else
+{
+    Console.WriteLine("Введено не 3-х значное число");
+}
---
+++ Example13/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного
+ числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.Write("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int Length = num.ToString().Length;
+if (Length >= 3)
+{
+    while (num > 999)
+    {
+        num = num /10;
+    }
+    int result = num % 10;
+    Console.WriteLine("Третья цифра введенного числа " + result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
